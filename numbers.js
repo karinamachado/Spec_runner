4719feb72208a6dcee383211cc0567d5fab7d7b7
Karina Machado <karinamachado@msn.com>
2016-08-04 17:56:00 -0300

    Update pass tests primitives numbers
    I made some corrections in the exercise, this is the reason to make the update

---
+++ numbers.js
@@ -0,0 +1,46 @@
+/*! strings v0.0.0 - MIT license */
+'use strict';
+
+
+//////////////////////////////////
+// YOUR CODE BELOW, VARIABLES ARE NOT ALLOWED FOR ANY PROBLEMS
+// THE POINT HERE IS TO IMPROVE YOUR UNDERSTANDING OF NUMBERS AS A RAW DATA TYPE
+//////////////////////////////////
+
+var numberChecker = function () {
+   
+   return 40;
+}
+numberChecker('The numberChecker is:');
+
+var numberSum = function () {
+  
+  // Return the sum of the numbers below
+    // 4234 and 07080
+  return 11314;
+}
+numberSum('The sum of the numbers is:');
+
+
+var numberModulus = function () {
+  // Return the remainder of the quotient of the 2 numbers below
+    // 11314, 37
+   
+  return 29;
+}
+numberModulus('The modulos of the numbers is:');
+
+var numberHelper1 = function () {
+  // Find and use a javascript method to convert the string below to a number and return it
+    // '8675309'
+    
+  return parseInt(8675309);
+}
+
+
+var numberHelper2 = function () {
+  // Convert the number below to a string
+    // 8675309
+  return '8675309'
+}
+
